1f9fbb280d58d821586fe9332e92119831c42c7b
Pavel <bruchkovskiiy@mail.ru>
2025-12-15 18:23:47 +0300

    Лабораторная 3 Раздел 3 Выполнен

--- app_store/urls.py
+++ app_store/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import product_view_json, shop_view
+from .views import product_view_json, shop_view, product_page_view
 
 urlpatterns = [
     path('product/', product_view_json),
     path('', shop_view),
+    path('product/<slug:page>.html', product_page_view),
+    path('product/<int:page>', product_page_view),
 ]
--- app_store/views.py
+++ app_store/views.py
@@ -16,6 +16,28 @@ def product_view_json(request):
                                                          'indent': 4})
 
 
+def product_page_view(request, page):
+    if request.method == "GET":
+        if isinstance(page, str):  # Проверяем, что в параметр page передали значение строкового типа
+            for data in DATABASE.values():  # Перебираем все товары (словари) в DATABASE
+                if data['html'] == page:  # Если значение переданного параметра совпадает именем html файла, получаемого по ключу
+                    with open(f'app_store/product/{page}.html', encoding="utf-8") as f:
+                        html_content = f.read()
+                        return HttpResponse(html_content)
+            # Если за всё время поиска не было совпадений, то значит по данному имени нет соответствующей
+            # страницы товара и можно вернуть ответ с ошибкой HttpResponse(status=404)
+            return HttpResponse(status=404)
+
+        elif isinstance(page, int):  # Ветка для обработки типа int
+            data = DATABASE.get(str(page))  # Получаем какой странице соответствует данный id
+            if data:  # Если по данному page было найдено значение
+                with open(f'app_store/product/{data["html"]}.html',
+                          encoding="utf-8") as f:  # Определяем название файла для открытия
+                    return HttpResponse(f.read())
+
+        return HttpResponse(status=404)
+
+
 def shop_view(request):
     if request.method == "GET":
         with open('app_store/shop.html', encoding="utf-8") as f:
