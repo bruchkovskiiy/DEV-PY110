ff5d25f9ccace4bdf8196f364dbfe42f8d48e1e5
Pavel <bruchkovskiiy@mail.ru>
2025-12-23 17:55:37 +0300

    Лабораторная 4 Раздел 6.2 Выполнен

--- app_store/views.py
+++ app_store/views.py
@@ -50,9 +50,17 @@ def product_page_view(request, page):
 
 def shop_view(request):
     if request.method == "GET":
-        return render(request,
-                      'app_store/shop.html',
-                      context={"products": DATABASE.values()})
+        # Обработка фильтрации из параметров запроса
+        category_key = request.GET.get("category")
+        if ordering_key := request.GET.get("ordering"):
+            if request.GET.get("reverse") in ('true', 'True'):
+                data = filtering_category(DATABASE, category_key, ordering_key, True)
+            else:
+                data = filtering_category(DATABASE, category_key, ordering_key)
+        else:
+            data = filtering_category(DATABASE, category_key)
+        return render(request, 'app_store/shop.html',
+                      context={"products": data})
 
 
 def cart_view(request):
