314d73e135f99699130b84db26e86787e6707249
Pavel <bruchkovskiiy@mail.ru>
2025-12-23 16:38:50 +0300

    Лабораторная 4 Раздел 4.1 Выполнен

--- app_store/urls.py
+++ app_store/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import product_view_json, shop_view, product_page_view, cart_view_json, cart_add_view_json, cart_del_view_json
+from .views import product_view_json, shop_view, product_page_view, cart_view_json, cart_add_view_json, cart_del_view_json, cart_view
 
 app_name = 'app_store'
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path('product/<slug:page>.html', product_page_view, name='product_page_view'),
     path('product/<int:page>', product_page_view),
     path('cart/json/', cart_view_json),
+    path('cart/', cart_view),
     path('cart/add/<id_product>', cart_add_view_json),
     path('cart/del/<id_product>', cart_del_view_json),
 ]
--- app_store/views.py
+++ app_store/views.py
@@ -58,6 +58,26 @@ def shop_view(request):
                       context={"products": DATABASE.values()})
 
 
+def cart_view(request):
+    if request.method == "GET":
+        username = ''
+        data = view_in_cart(username)[username]  # Получаем корзину пользователя username
+
+        products = []  # Список продуктов
+        for product_id, quantity in data['products'].items():
+            product = DATABASE[product_id]  # Получаем информацию о продукте
+            # TODO в словарь product под ключом "quantity" запишите текущее значение количества товара в корзине
+            product["quantity"] = quantity  # Реализуйте
+            # TODO в словарь product под ключом "price_total" посчитайте и запишите общую стоимость товара как произведение
+            #  его количества в корзине на цену с учетом скидки ('price_after'). Значение цены "price_total" приведите к формату
+            #  2 символов после запятой
+            product["price_total"] = f"{quantity * product['price_after']:.2f}"  # Реализуйте
+            # TODO добавьте словарь product в конец списка products
+            products.append(product)  # Реализуйте
+
+        return render(request, "app_store/cart.html", context={"products": products})
+
+
 def cart_view_json(request):
     if request.method == "GET":
         username = ''
