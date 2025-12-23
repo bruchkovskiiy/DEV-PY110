6a908102562175e8cdb3d3fc04887ed03f6b057a
Pavel <bruchkovskiiy@mail.ru>
2025-12-23 16:21:04 +0300

    Лабораторная 4 Раздел 3.3 Выполнен

--- app_store/urls.py
+++ app_store/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import product_view_json, shop_view, product_page_view, cart_view_json, cart_add_view_json, cart_del_view_json
 
+app_name = 'app_store'
+
 urlpatterns = [
     path('product/', product_view_json),
     path('', shop_view),
-    path('product/<slug:page>.html', product_page_view),
+    path('product/<slug:page>.html', product_page_view, name='product_page_view'),
     path('product/<int:page>', product_page_view),
     path('cart/json/', cart_view_json),
     path('cart/add/<id_product>', cart_add_view_json),
