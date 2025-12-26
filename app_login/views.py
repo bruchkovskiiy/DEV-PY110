329451eae8a624a561e8e37a0e47f149cf0dbe49
Pavel <bruchkovskiiy@mail.ru>
2025-12-26 16:43:28 +0300

    Лабораторная 5 Раздел 2.3 Выполнен

--- app_login/views.py
+++ app_login/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
 
 
 def login_view(request):
     if request.method == "GET":
         return render(request, "login/login.html")
+
+    if request.method == "POST":
+        data = request.POST  # Получаем данный из post запроса
+        user = authenticate(username=data["username"], password=data["password"])  # Понимаем, что за пользователь перед нами
+        if user:  # Если пользователь есть в базе
+            login(request, user)  # Авторизируем пользователя
+            return redirect("/")  # Перенаправляем пользователя на стартовую страницу
+        # Иначе заново показываем форму авторизации
+        return render(request, "login/login.html", context={"error": "Неверные данные"})
