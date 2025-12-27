25e6b5f2be74b7ea74403e1f7d1bcf2086f8d137
Pavel <bruchkovskiiy@mail.ru>
2025-12-27 19:04:07 +0300

    Лабораторная 6 Этап 3 Выполнен

--- app_login/views.py
+++ app_login/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
 from logic.control_cart import view_in_cart
+from logic.control_wishlist import view_in_wishlist
 
 
 def login_view(request):
@@ -22,3 +23,19 @@ def logout_view(request):
     if request.method == "GET":
         logout(request)  # Функция разлогинивает пользователя
         return redirect("/") # TODO Верните редирект на стартовую страницу
+
+
+def login_view(request):
+    if request.method == "GET":
+        return render(request, "login/login.html")
+
+    if request.method == "POST":
+        data = request.POST  # Получаем данный из post запроса
+        user = authenticate(username=data["username"], password=data["password"])  # Понимаем, что за пользователь перед нами
+        if user:  # Если пользователь есть в базе
+            login(request, user)  # Авторизируем пользователя
+            view_in_cart(user.username)  # Получаем корзину пользователя, если её нет, то создаем её
+            view_in_wishlist(user.username)  # TODO добавить пользователя в базу избранное view_in_wishlist(user.username)
+            return redirect("/")  # Перенаправляем пользователя на стартовую страницу
+        # Иначе заново показываем форму авторизации
+        return render(request, "login/login.html", context={"error": "Неверные данные"})
--- app_wishlist/views.py
+++ app_wishlist/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse, HttpResponseNotFound
+from app_store.models import DATABASE
+from django.contrib.auth import get_user
+from logic.control_wishlist import view_in_wishlist, add_to_wishlist, remove_from_wishlist
+from django.contrib.auth.decorators import login_required
 
 
 def wishlist_view(request):
     if request.method == "GET":
-        return render(request, 'app_wishlist/wishlist.html', {}) # TODO прописать отображение избранного. Путь до HTML - app_wishlist/wishlist.html
+        username = get_user(request).username
+        data = view_in_wishlist(username)[username]  # TODO получить продукты из избранного для пользователя, используя view_in_wishlist
+
+        products = [DATABASE[product_id] for product_id in data['products']]
+        # TODO сформировать список словарей продуктов с их характеристиками. Пройдитесь по id продуктам в data
+        #  получите словари с характеристиками продуктов по их id и запишите в список products
+
+        return render(request, 'app_wishlist/wishlist.html', context={"products": products})
