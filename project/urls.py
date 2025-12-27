581ad58a37bb74e25a8799b3629c921f710b1c59
Pavel <bruchkovskiiy@mail.ru>
2025-12-27 18:30:06 +0300

    Лабораторная 6 Этап 2 Выполнен

--- project/urls.py
+++ project/urls.py
@@ -55,4 +55,5 @@ urlpatterns = [
     path('', include('app_weather.urls')),
     path('', include('app_store.urls')),
     path('login/', include('app_login.urls')),
+    path('wishlist/', include('app_wishlist.urls')),  # TODO Зарегистрируйте маршруты приложения так чтобы обработчик избранного обрабатывался по пути wishlist/
 ]
