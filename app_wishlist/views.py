88e44b2311f320f3dcce51540680fcd26504b963
Pavel <bruchkovskiiy@mail.ru>
2025-12-27 19:45:58 +0300

    Лабораторная 6 Этап 5 Выполнен

--- app_wishlist/views.py
+++ app_wishlist/views.py
@@ -16,3 +16,56 @@ def wishlist_view(request):
         #  получите словари с характеристиками продуктов по их id и запишите в список products
 
         return render(request, 'app_wishlist/wishlist.html', context={"products": products})
+
+
+def wishlist_view_json(request):
+    """
+    Просмотр всех продуктов в избранном для пользователя и возвращение этого в JSON
+    """
+    if request.method == "GET":
+        username = get_user(request).username  # from django.contrib.auth import get_user
+        data = view_in_wishlist(username)[username]  # TODO получите данные о списке товаров в избранном у пользователя view_in_wishlist(username)[username]
+        return JsonResponse(data, json_dumps_params={'ensure_ascii': False,
+                                                     'indent': 4})
+
+
+def wishlist_add_view_json(request, id_product: str):
+    """
+    Добавление продукта в избранное и возвращение информации об успехе или неудаче в JSON
+    """
+    if request.method == "GET":
+        username = get_user(request).username
+        result = add_to_wishlist(id_product, username)  # TODO вызовите обработчик add_to_wishlist(id_product, username) добавляющий продукт
+        if result:
+            return JsonResponse({"answer": "Продукт успешно добавлен в избранное"},
+                                json_dumps_params={'ensure_ascii': False})  # TODO верните JsonResponse с ключом "answer" и значением "Продукт успешно добавлен в избранное"
+
+        return JsonResponse({"answer": "Неудачное добавление в избранное"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})  # TODO верните JsonResponse с ключом "answer" и значением "Неудачное добавление в избранное" и параметром status=404
+
+
+def wishlist_del_view_json(request, id_product: str):
+    """
+    Удаление продукта из избранного и возвращение информации об успехе или неудаче в JSON
+    """
+    if request.method == "GET":
+        username = get_user(request).username
+        result = remove_from_wishlist(id_product, username)  # TODO вызовите обработчик remove_from_wishlist(id_product, username) удаляющий продукт из избранного
+        if result:
+            return JsonResponse({"answer": "Продукт успешно удалён из избранного"},
+                                json_dumps_params={'ensure_ascii': False})  # TODO верните JsonResponse с ключом "answer" и значением "Продукт успешно удалён из избранного"
+
+        return JsonResponse({"answer": "Неудачное удаление из избранного"},
+                            status=404,
+                            json_dumps_params={'ensure_ascii': False})  # TODO верните JsonResponse с ключом "answer" и значением "Неудачное удаление из избранного" и параметром status=404
+
+
+def wishlist_remove_view(request, id_product):
+    if request.method == "GET":
+        username = get_user(request).username
+        result = remove_from_wishlist(id_product, username)  # TODO Вызвать функцию удаления из корзины
+        if result:
+            return redirect("app_wishlist:wishlist_view")  # TODO Вернуть перенаправление на корзину
+
+        return HttpResponseNotFound("Неудачное удаление из избранного")
